5d9b41029ad8b43333f8fe180ecd220f5e2d8fa0
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-05 13:36:21 -0300

    factorial done

---
+++ 4-factorial/index.js
@@ -0,0 +1,15 @@
+let number = 5
+
+function calculateFactorial(number) {
+  let factorial = 1
+  for (let i = 1; i <= number; i++) {
+    if (number === 0) {
+      return (factorial = 1)
+    } else {
+      factorial *= i
+    }
+  }
+  return factorial
+}
+
+console.log(calculateFactorial(number))

45e47f741cbdd8bf70e30eaff20e107503567402
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-05 20:10:42 -0300

    sorting of array done

---
+++ 7-orderingArray/index.js
@@ -0,0 +1,19 @@
+let array = [60, 0, 20, 2 ,0]
+
+function sort(array){
+  const n = array.length
+
+  for(let i = 0; i< n-1; i++){
+    for(let j = 0; j< n-1 -i; j++){
+      console.log(array)
+      if(array[j] > array[j + 1]){
+        const temp = array[j]
+        array[j] = array[j +1]
+        array[j + 1] = temp
+      }
+    }
+  }
+  return array
+}
+
+console.log(sort(array))

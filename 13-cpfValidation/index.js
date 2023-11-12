05b5bd976b177cc9d1741047eb19bf789402d3b8
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-12 12:54:44 -0300

    cpf validation done

--- 13-cpfValidation/index.js
+++ 13-cpfValidation/index.js
@@ -1,47 +1,64 @@
-// first: to verify the string using regexp to clean and take out the dots and characters
+// first: to verify if the cpf variable are an string or a number, because we need it be an array of numbers to use it into our validation function
 // second: loop the cleaned array, calculate the next number and increase it
 // third: redo another loop and increase the next number
 
-let number = '111444777'
-let splitNumber = number.split('.')
-let arrayVerified = splitNumber.join('')
+// function to transform from a string or number to an array
+function transformIntoAnArray(cpf) {
+  let array
+  if (typeof cpf === 'string') {
+    let verifyCpf = cpf.split('.')
+    let verifiedCpf = parseInt(verifyCpf.join(''))
+    // transform into an array
+    array = Array.from(String(verifiedCpf), Number)
+  } else if (typeof cpf === 'number') {
+    array = Array.from(String(cpf), Number)
+  }
+  return array
+}
 
-function checkCpf(array) {
-  let validationNumber = [10,9,8,7,6,5,4,3,2]
-  let cpfVerified = []
+// function to validate the CPF sequence
+function validationCpf(cpf) {
+  // generating the first number
+  let toMultiply = [10, 9, 8, 7, 6, 5, 4, 3, 2]
   let sum = 0
-
-  // first number validation
-  for (let i = 0; i < array.length; i++) {
-    for(let j = 0 ; j < validationNumber.length; j++){
-      let newNumber = array[i] * validationNumber[j]
-      sum += newNumber
-      
-    }
+  for (let i = 0; i < toMultiply.length && i < cpf.length; i++) {
+    sum += toMultiply[i] * cpf[i]
     console.log(sum)
-    
   }
 
   let firstNumber = sum % 11
-
   if (firstNumber < 2) {
     firstNumber = 0
   } else {
-    firstNumber = firstNumber - 8
+    firstNumber = 11 - firstNumber
+    console.log(`first number ${firstNumber}`)
   }
-  cpfVerified.push(firstNumber)
+  cpf.push(firstNumber)
 
-  // second number validation
-  validationNumber = [2,3,4,5,6,7,8,9,10,11]
-  for(let i = 0 ; i < cpfVerified.length; i++){
-    sum += parseInt(cpfVerified[i])
-    newNumber = 
+  // generating the second number
+  toMultiply = [11, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+  sum = 0
+  for (let i = 0; i < toMultiply.length && i < cpf.length; i++) {
+    sum += toMultiply[i] * cpf[i]
     console.log(sum)
   }
+  let secondNumber = sum % 11
+  if (secondNumber < 2) {
+    secondNumber = 0
+  } else {
+    secondNumber = 11 - secondNumber
+  }
+  cpf.push(secondNumber)
 
-  console.log(cpfVerified)
+  return cpf
 }
 
-console.log(checkCpf(arrayVerified))
+// testing
+let cpf = '111.444.777'
+let cpf2 = 111444777
 
-console.log(45 % 11)
+let firstCpf = transformIntoAnArray(cpf)
+console.log(validationCpf(firstCpf))
+
+let secondCpf= transformIntoAnArray(cpf2)
+console.log(validationCpf(secondCpf))

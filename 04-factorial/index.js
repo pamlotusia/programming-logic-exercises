3ca1677d3d2b7b6b2b44bbdee789a892493b9dc3
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-13 20:36:41 -0300

    starting factorial

--- 04-factorial/index.js
+++ 04-factorial/index.js
@@ -3,7 +3,7 @@ let number = 5
 function calculateFactorial(number) {
   let factorial = 1
   for (let i = 1; i <= number; i++) {
-    if (number === 0) {
+    if (number <= 0) {
       return (factorial = 1)
     } else {
       factorial *= i

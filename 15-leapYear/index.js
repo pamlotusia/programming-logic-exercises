cd2a50aee3ec09c4df7eb055d38483e73144e430
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-13 21:01:31 -0300

    leap year done

--- 15-leapYear/index.js
+++ 15-leapYear/index.js
@@ -1,3 +1,15 @@
-let today = new Date()
+function leapYear(year){
+  if (year % 4 == 0){
+    if(!year % 100 == 0 || year % 400 == 0 ){
+    }
+    console.log('leap year', year)
+  } else {
+    console.log('it is not a leap year', year)
+  }
+}
 
-console.log(today.getFullYear())
+leapYear(2014)
+leapYear(2000)
+leapYear(2010)
+leapYear(2012)
+leapYear(2024)

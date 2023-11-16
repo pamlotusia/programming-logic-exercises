5f592d8b1890d9660db91539d4681d67655d8195
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-16 20:58:15 -0300

    find max value using forEach done

--- 21.4-forEachFindMax/index.js
+++ 21.4-forEachFindMax/index.js
@@ -1,24 +1,13 @@
-let array = [1,2,3,4,5]
-array.forEach((element) => {
-  console.log(element)
-})
+// I initialized maxValue with the first element value because that makes sure even the negative numbers will be validated right
+function maxValue(array){
+  let maxValue = array[0]
+  array.forEach((value)=>{
+    if(value > maxValue ){
+      maxValue = value
+    }
+  })
+  return maxValue
+}
 
-array.forEach((element, index)=>{
-  console.log(`${element} is in position ${index}`)
-})
-
-array.forEach((element, index, array)=>{
-  console.log(`${element} is in position ${index} and makes part of the array ${array}`)
-})
-
-// using array of objects 
-let arrayObjects = [{name: 'Pam'}, {name: 'Paul'}, {name: 'Peter'}, {age: 21}]
-
-// because we are accessing an object by object in this array, we need to describe which property we want to access too. Otherwise it going to show 'object Object'
-// of course, all the objects need to have the same key to be accessed, or it going to return 'undefined' on the one that doesn't have it. 
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element}`)
-})
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element.name}`)
-})
+let array = [-1,-2,-8]
+console.log(maxValue(array))

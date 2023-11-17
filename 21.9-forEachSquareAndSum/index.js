2fc174fb5b9913ddde75b2aeb8fbf5d7a369170a
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-16 22:35:05 -0300

    square and sum using forEach done

--- 21.9-forEachSquareAndSum/index.js
+++ 21.9-forEachSquareAndSum/index.js
@@ -1,24 +1,10 @@
-let array = [1,2,3,4,5]
-array.forEach((element) => {
-  console.log(element)
-})
+function squareAndSum(array){
+  let sum = 0
+  array.forEach((number)=>{
+    number = number * number
+    sum += number
+  })
+  return sum
+}
 
-array.forEach((element, index)=>{
-  console.log(`${element} is in position ${index}`)
-})
-
-array.forEach((element, index, array)=>{
-  console.log(`${element} is in position ${index} and makes part of the array ${array}`)
-})
-
-// using array of objects 
-let arrayObjects = [{name: 'Pam'}, {name: 'Paul'}, {name: 'Peter'}, {age: 21}]
-
-// because we are accessing an object by object in this array, we need to describe which property we want to access too. Otherwise it going to show 'object Object'
-// of course, all the objects need to have the same key to be accessed, or it going to return 'undefined' on the one that doesn't have it. 
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element}`)
-})
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element.name}`)
-})
+console.log(squareAndSum([2,3])) //13

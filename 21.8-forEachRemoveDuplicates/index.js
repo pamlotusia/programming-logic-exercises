7453092ee90238db40a65c806e2484f7224293c5
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-16 22:25:48 -0300

    remove duplicates using forEach done

--- 21.8-forEachRemoveDuplicates/index.js
+++ 21.8-forEachRemoveDuplicates/index.js
@@ -1,24 +1,16 @@
-let array = [1,2,3,4,5]
-array.forEach((element) => {
-  console.log(element)
-})
+function removeDuplicates(array) {
+  let noDuplicates = []
+  array.forEach(element => {
+    if (!noDuplicates.includes(element)) {
+      noDuplicates.push(element)
+    }
+  })
 
-array.forEach((element, index)=>{
-  console.log(`${element} is in position ${index}`)
-})
+  array = noDuplicates
 
-array.forEach((element, index, array)=>{
-  console.log(`${element} is in position ${index} and makes part of the array ${array}`)
-})
+  return array
+}
 
-// using array of objects 
-let arrayObjects = [{name: 'Pam'}, {name: 'Paul'}, {name: 'Peter'}, {age: 21}]
+let test = [1, 1, 2, 2, 3, 4, 10, 10]
 
-// because we are accessing an object by object in this array, we need to describe which property we want to access too. Otherwise it going to show 'object Object'
-// of course, all the objects need to have the same key to be accessed, or it going to return 'undefined' on the one that doesn't have it. 
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element}`)
-})
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element.name}`)
-})
+console.log(removeDuplicates(test))

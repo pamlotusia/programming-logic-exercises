1c423482c587f083348eba1ec26bb2c1a5a184b2
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-16 21:55:02 -0300

    count words using forEach done

--- 21.6-forEachCountWords/index.js
+++ 21.6-forEachCountWords/index.js
@@ -1,24 +1,19 @@
-let array = [1,2,3,4,5]
-array.forEach((element) => {
-  console.log(element)
-})
+function countWords(array) {
+  let totalWords = 0
+  let totalByPhrase = {}
 
-array.forEach((element, index)=>{
-  console.log(`${element} is in position ${index}`)
-})
+  array.forEach((phrase, index) => {
+    let splitPhrase = phrase.split(' ')
+    let totalWordsByPhrase = splitPhrase.length
 
-array.forEach((element, index, array)=>{
-  console.log(`${element} is in position ${index} and makes part of the array ${array}`)
-})
+    totalWords += totalWordsByPhrase
 
-// using array of objects 
-let arrayObjects = [{name: 'Pam'}, {name: 'Paul'}, {name: 'Peter'}, {age: 21}]
+    totalByPhrase[`sentence${index + 1}`] = totalWordsByPhrase
+  })
 
-// because we are accessing an object by object in this array, we need to describe which property we want to access too. Otherwise it going to show 'object Object'
-// of course, all the objects need to have the same key to be accessed, or it going to return 'undefined' on the one that doesn't have it. 
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element}`)
-})
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element.name}`)
-})
+  return [totalWords, totalByPhrase]
+}
+
+let arrayTest = ['hello friend', 'I am so happy for my progress']
+
+console.log(countWords(arrayTest))

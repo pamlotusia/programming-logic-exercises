10bc11ceb494f2db8beeb1270ffd370a2eb2050d
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-11-16 21:06:46 -0300

    convert to fahrenheit using forEach done

--- 21.5-forEachConvertFahrenheit/index.js
+++ 21.5-forEachConvertFahrenheit/index.js
@@ -1,24 +1,13 @@
-let array = [1,2,3,4,5]
-array.forEach((element) => {
-  console.log(element)
-})
+function toFahrenheit(array){
+  let fahrenheit = []
+  array.forEach((number)=>{
+    number = (number * (9/5)) + 32
+    console.log(number)
+    fahrenheit.push(number)
+  })
+  return fahrenheit
+}
 
-array.forEach((element, index)=>{
-  console.log(`${element} is in position ${index}`)
-})
+let arrayCelsius = [0, 36, 20] // [32, 96.8, 68]
+console.log(toFahrenheit(arrayCelsius))
 
-array.forEach((element, index, array)=>{
-  console.log(`${element} is in position ${index} and makes part of the array ${array}`)
-})
-
-// using array of objects 
-let arrayObjects = [{name: 'Pam'}, {name: 'Paul'}, {name: 'Peter'}, {age: 21}]
-
-// because we are accessing an object by object in this array, we need to describe which property we want to access too. Otherwise it going to show 'object Object'
-// of course, all the objects need to have the same key to be accessed, or it going to return 'undefined' on the one that doesn't have it. 
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element}`)
-})
-arrayObjects.forEach((element)=>{
-  console.log(`current element ${element.name}`)
-})

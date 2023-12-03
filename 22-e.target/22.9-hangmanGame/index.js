c5ebce6d8f5a55d8361529ef53cb56a91bfcca79
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-12-03 09:17:35 -0300

    first version of hangman game (there are mistakes)

--- 22-e.target/22.9-hangmanGame/index.js
+++ 22-e.target/22.9-hangmanGame/index.js
@@ -1,7 +1,43 @@
-const ul = document.querySelectorAll('ul *')
+const table = document.querySelector('table')
+const hiddenWord = document.querySelector('#hidden-word')
+const score = document.querySelector('#score span')
+let scoreNumber = parseInt(score.textContent)
+const changes = document.querySelector('#changes span')
+let changesNumber = parseInt(changes.textContent)
+let creatingWord = document.querySelector('#word')
+let arrayCreatingWord = creatingWord.textContent.split('')
 
-ul.forEach((item)=>{
-  item.addEventListener('click', (e)=>{
-    console.log(e.target.textContent)
-  })
+// word chosen
+const word = 'palavra'
+hiddenWord.textContent = word
+// hiddenWord.style.visibility = 'hidden'
+let arrayWord = word.split('')
+
+// board game
+table.addEventListener('click', function (event) {
+  let letter = event.target.textContent
+  if (event.target.tagName === 'TD') {
+
+
+    if (arrayWord.includes(letter)) {
+
+      let index = arrayWord.indexOf(letter)
+      arrayCreatingWord.splice(index, 0, letter)
+
+      let showWord = arrayCreatingWord.join('')
+      creatingWord.textContent = showWord
+
+      console.log(showWord)
+    } else {
+      scoreNumber++
+      score.textContent = scoreNumber
+      changesNumber--
+      changes.textContent = changesNumber
+      if (scoreNumber == 6) {
+        alert('voce perdeu')
+      }
+
+      console.log(scoreNumber)
+    }
+  }
 })

d68d03d113de9af17a80b216030220326897d4a3
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-12-03 20:56:01 -0300

    calculator done

--- 22-e.target/22.18-calculator/index.js
+++ 22-e.target/22.18-calculator/index.js
@@ -1,12 +1,5 @@
-const table = document.querySelector('table')
-const btn = document.querySelector('button')
-
-btn.addEventListener('click', () => {
-  let row = document.createElement('tr')
-  table.appendChild(row)
-  for (let i = 0; i < 6; i++) {
-    let cell = document.createElement('td')
-    cell.textContent = 10
-    row.appendChild(cell)
+document.body.addEventListener('click', (e)=>{
+  if(e.target.tagName === 'BUTTON'){
+    console.log(e.target.textContent)
   }
 })

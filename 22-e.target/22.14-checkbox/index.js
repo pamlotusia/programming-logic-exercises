6979546245abc977b2de00ef2d6bfcf1ac4ce856
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-12-03 20:08:55 -0300

    checkbox done

--- 22-e.target/22.14-checkbox/index.js
+++ 22-e.target/22.14-checkbox/index.js
@@ -1,4 +1,14 @@
 const checkbox = document.querySelector('input')
 let message = document.querySelector('#message')
 
-message.textContent = 'Hi darling'
+message.textContent = 'Empty input'
+
+checkbox.addEventListener('click', changePhrase)
+
+function changePhrase(e){
+  if(e.target.checked){
+    message.textContent = 'You checked the input'
+  } else {
+    message.textContent = 'Empty input'
+  }
+}

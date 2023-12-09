880b684e9b17fb9426ddb08defc6a73ae98a111c
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-12-08 21:08:15 -0300

    calculator improved

--- 22-e.target/22.19-basicOperations/index.js
+++ 22-e.target/22.19-basicOperations/index.js
@@ -1,12 +1,74 @@
-const table = document.querySelector('table')
-const btn = document.querySelector('button')
+let num1 = ''
+let num2 = ''
+let operator = ''
+let resultDisplay = document.querySelector('#result')
+let equal = document.querySelector('#equal')
 
-btn.addEventListener('click', () => {
-  let row = document.createElement('tr')
-  table.appendChild(row)
-  for (let i = 0; i < 6; i++) {
-    let cell = document.createElement('td')
-    cell.textContent = 10
-    row.appendChild(cell)
+// verify if the e.target is a number or an operator
+document.body.addEventListener('click', (e)=>{
+  if(e.target.classList.contains('number')){
+    handleNumberClick(e.target.textContent)
+    visor(e.target.textContent)
+  } else if (e.target.classList.contains('operator')){
+    handleOperatorClick(e.target.textContent)
   }
+  
 })
+
+function handleNumberClick(number){
+  if(!operator){
+    num1+= number
+    console.log(typeof num1)
+
+  } else {
+    num2+= number
+  }
+  updateDisplay()
+}
+
+function handleOperatorClick(op){
+  if(num1 !== ''){
+    operator = op
+  }
+}
+
+// shows the current number
+function visor(element){
+  resultDisplay.value = element
+}
+
+// shows the result
+function updateDisplay(){
+ let result
+ if(operator === '+') {
+  result = sum(num1, num2)
+ } else if (operator === '-'){number
+  result = sub(num1, num2)
+ } else if (operator === '*'){
+  result = multiply(num1, num2)
+ } else if( operator === '/'){
+  result = divide(num1, num2)
+ }
+
+ resultDisplay.value = result
+
+ if(resultDisplay.value === 'undefined'){
+  resultDisplay.value = ''
+ }
+ return result 
+}
+equal.addEventListener('click', updateDisplay)
+
+// operations
+function sum(number1, number2){
+  return parseFloat(number1) + parseFloat(number2)
+}
+function sub(number1, number2){
+  return parseFloat(number1) - parseFloat(number2)
+}
+function multiply(number1, number2){
+  return parseFloat(number1) * parseFloat(number2)
+}
+function divide(number1, number2){
+  return parseFloat(number1) / parseFloat(number2)
+}

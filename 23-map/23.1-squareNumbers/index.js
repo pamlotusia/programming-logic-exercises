0b1838098cef748e7322db2867fc63d9a40ceeb2
Pamela Lima <pamela.jesus.lima@gmail.com>
2023-12-10 21:19:05 -0300

    nine exercises done

---
+++ 23-map/23.1-squareNumbers/index.js
@@ -0,0 +1,6 @@
+const array = [1, 2, 3, 4, 5]
+const square = array.map((number) => {
+  return number * 2
+})
+
+console.log(square)
--- 23-map/23.6-evenWordLength/index.js
+++ 23-map/23.6-evenWordLength/index.js
@@ -1,54 +1,10 @@
-const array1 = [1, 4, 9, 16]
-
-const map1 = array1.map(n => n * 2)
-console.log(map1) //2, 8, 18, 32
-
-// square numbers
-const numbers = [1, 4, 9]
-const roots = numbers.map(number => Math.sqrt(number))
-console.log(numbers) //the parent array still unchanged
-console.log(roots) // 1, 2, 3
-
-// Using map to reform objects in an array
-const kvArray = [
-  { key: 1, value: 10 },
-  { key: 2, value: 20 },
-  { key: 3, value: 30 }
-]
-
-const reformattedArray = kvArray.map(({key, value})=> ({[key]: value})) // take notes: the parameter need to be the same shape as the return value
-
-console.log(kvArray) 
-// [
-//   { key: 1, value: 10 },
-//   { key: 2, value: 20 },
-//   { key: 3, value: 30 }
-// ]
-
-console.log(reformattedArray)
-// [{1: 10}, {2: 20}, {3: 30}]
-
-console.log(['1', '2', '3'].map((n)=> parseInt(n)))
-
-// Unlike parseInt(), Number() will return a float or (resolved) exponential notation
-console.log(["1.1", "2.2e2", "3e300"].map(Number))
-
-// mapped array contains undefined
-const numbers2 = [1, 2, 3, 4]
-const filteredNumbers = numbers2.map((num, index) => {
-  if (index < 3) {
-    return num
+const strings = ['hello', 'world', 'good']
+const evenLength = strings.map((word)=>{
+  if(word.length % 2 == 0){
+    return word = true
+  } else {
+    return word = false
   }
 })
-console.log(filteredNumbers) // 1, 2, 3, undefined
 
-// Side-effectfull mapping
-// The callback can have side effects.
-const cart = [5, 15, 25]
-let total = 0
-const withTax = cart.map((cost) => {
-  total += cost
-  return cost * 1.2
-})
-console.log(withTax)
-console.log(total)
+console.log(evenLength)
